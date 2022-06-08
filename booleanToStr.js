e9776b173179047b33eff29f343794cf16a994fa
Macdonald Kawara <84657473+mackawara@users.noreply.github.com>
2022-06-08 17:18:51 +0200

    Create booleanToStr.js

---
+++ booleanToStr.js
@@ -0,0 +1,14 @@
+/* Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+
+*/
+
+//Solution
+function booleanToString(b){
+  //your code here
+  return b===true?"true":"false"
+}
+
+/*Lessons
+How to use ternary operator */
