1adb8c6ef56a73f530c1e3f6da725b3af16ed29e
Macdonald Kawara <84657473+mackawara@users.noreply.github.com>
2022-06-21 19:14:51 +0200

    used split to create array
    used fiter with regex to filter the vowels

---
+++ vowelCount.js
@@ -0,0 +1,16 @@
+
+// Return the number (count) of vowels in the given string.
+
+//We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+//The input string will only consist of lower case letters and/or spaces.
+
+
+function getCount(str) {
+  const regex= /a|e|i|o|u/i
+const arr=str.split("")
+const vowels=arr.filter((char)=>{
+return regex.test(char)
+})
+  return vowels.length
+}
