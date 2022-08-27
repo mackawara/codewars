81f5ff5098918fb9a708b2c22f10e70a2f8adcb4
mackawara <mkawara@outlook.com>
2022-08-27 07:23:30 +0200

    feat : mvp working

--- 6kyu/duplicate.js
+++ 6kyu/duplicate.js
@@ -11,6 +11,26 @@ Assertion messages may be unclear about what they display in some languages. If 
  */
 //Solution
 //1. Split the sring into aarray
-//2. use array loop methods find? find?count? to count the numbe of appearances
+//2. use array loop methods find? find?count? to count the numbe of appearances or custom function
 //3. convert if length >1
 //4. return string
+
+function duplicateEncode(word){
+    const array1=word.toLowerCase().split("")
+    function getCount(letter,array){
+        let count=0
+        array.forEach(item=>{
+            if (letter==item)count++
+           // console.log(count)
+        
+        })
+        console.log(count)
+       return count
+       }
+ return array1.map(letter=>{
+     if (getCount(letter,array1)>1)return ")"
+     else return "("   
+    }).join("")
+    
+}
+console.log(duplicateEncode("success"))
